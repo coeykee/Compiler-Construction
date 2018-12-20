7aeed9da19b0cb3b8ae02becd6441647b6965257
coeykee <draut900@gmail.com>
2018-12-20 23:34:26 +0530

    output .txt added

--- week01/simple/mainp.c
+++ week01/simple/mainp.c
@@ -1,3 +1,6 @@
+
+mainp.c
+
 #include<stdio.h>
 #define opit(x, y) (x+y*3.14)
 int main()
@@ -7,4 +10,12 @@ int main()
 		return 0;
 	}
 	
+output screen:
 
+dnyanesh@computer:~/58/Compiler-Construction/week01/simple$ gcc -Wall -save-temps mainp.c -o output
+dnyanesh@computer:~/58/Compiler-Construction/week01/simple$ ls
+mainp.c  mainp.i  mainp.o  mainp.s  output
+dnyanesh@computer:~/58/Compiler-Construction/week01/simple$ ls -sh
+total 48K
+4.0K mainp.c   16K mainp.i  4.0K mainp.o  4.0K mainp.s   20K output
+dnyanesh@computer:~/58/Compiler-Construction/week01/simple$ 
